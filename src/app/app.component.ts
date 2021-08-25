6d7b150c2c1eb68bf442a6b9fe55c0879d068987
José Manuel Cota <jmcota@ontica.org>
2021-08-24 21:08:18 -0400

    Observables, subcriptions, subscribe/unsubscribe, OnInit/OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,58 @@
 
-import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component(
   {
     selector: 'app-root',
     templateUrl: './app.component.html'
   })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
-  constructor(private httpClient: HttpClient) {}
+  tablaAuxiliares = 'Aquí va la tabla de auxiliares';
 
   title = 'Sesiones de transferencia tecnológica Banobras';
 
   year = 2021;
+
+  subscription: Subscription | undefined;
+
+  constructor(private httpClient: HttpClient) {}
+
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+
+  ngOnInit(): void {
+    this.setTablaAuxiliares();
+  }
+
+
+  private setTablaAuxiliares(): void {
+    const accountsChartUID = 'b2328e67-3f2e-45b9-b1f6-93ef6292204e';
+    const keywords = 'Cristina lechuga García';
+
+    const url = `http://172.27.207.97/sicofin/api/v2/financial-accounting` +
+                `/${accountsChartUID}/subsidiary-accounts/${keywords}`;
+
+    console.log('antes del get');
+
+    this.subscription = this.httpClient.get(url).subscribe(
+      data => {
+        console.log(data);
+        console.log('regreso satisfactorio');
+        this.tablaAuxiliares = JSON.stringify(data);
+      },
+      error => console.log('hubo un problema en la llamada a la API', error)
+    );
+
+    console.log('después del get');
+  }
 
   public nextYear(): number {
     return this.year;
@@ -22,22 +60,6 @@ export class AppComponent {
 
   public changeTitle(year: number): void {
     this.title = "Siguiente en " + year + " " + new Date().getSeconds();
-  }
-
-  
-
-  getTablaAuxiliares() {
-    const accountsChartUID = 'b2328e67-3f2e-45b9-b1f6-93ef6292204e';
-    const keywords = 'Cristina lechuga García';
-
-    const url = `http://172.27.207.97/sicofin/api/v2/financial-accounting` +
-                `/${accountsChartUID}/subsidiary-accounts/${keywords}`;
-
-    this.httpClient.get(url).pipe(
-      tap((data)=> console.log("Mis datos" , data))
-    );
-
-    return "mi tabla de auxiliares";
   }
 
 }
